e2b381422544ca69cdce9cdea572dd68a0a1311d
Nomada Dev <tec@nomada.gt>
2019-10-21 13:18:27 -0600

    visualizando un grupo de montons de dinero

--- src/datavis-dinero.js
+++ src/datavis-dinero.js
@@ -23,94 +23,97 @@ data["iso_basis_y"] = new Phaser.Math.Vector2(Math.sin(data.iso_angle_x), -Math.
 data["iso_basis_x"] = new Phaser.Math.Vector2(Math.cos(data.iso_angle_x), Math.cos(data.iso_angle_y));
 
 var utils = {
-    rnd: new Phaser.Math.RandomDataGenerator(0),
-    CuboDeDinero: function (N, scene, x, y) {
-        var v        =     data.orig_ancho*data.orig_grosor*data.orig_largo, 
-            V        =     N*v, 
-            pre_x    = Math.pow(V, 1/3) / data.orig_largo,
-            final_x  =     Math.max(1, Math.ceil(pre_x)),
-            pre_y    = Math.sqrt( V / (data.orig_largo * final_x) )/data.orig_ancho,
-            final_y  = Math.max(1, Math.ceil(pre_y)),
-            final_z  = Math.ceil(V/(data.orig_grosor * final_x * data.orig_largo * final_y * data.orig_ancho)) ;
-        var ix =0, iy = 0;
-        iy = -data.iso_basis_y.dot(new Phaser.Math.Vector2(0, (final_y-1)*data.orig_ancho*data.draw_scale));
-        var alto  = iy + final_z*data.orig_grosor*data.draw_scale + data.iso_basis_y.dot(new Phaser.Math.Vector2((final_x+1)*data.orig_largo*data.draw_scale, 0)) + 5; 
-        var ancho = data.iso_basis_x.dot(new Phaser.Math.Vector2((final_x)*data.orig_largo*data.draw_scale, (final_y)*data.orig_ancho*data.draw_scale)); 
-        this.gameObject = new Phaser.GameObjects.RenderTexture(scene, x, y, ancho, alto);
-        this.graphics = scene.add.graphics().setVisible(false);
-
-        this.graphics.setTexture("billete_lado");
-        this.graphics.fillStyle(0xdadb5e, 1);
-
-        this.render = function () {
-            for (var x = 1; x <= final_x; x++) {
-                for (var y = 1; y <= final_y; y++) {
-                    var xyvector = new Phaser.Math.Vector2((x-1)*data.orig_largo*data.draw_scale,  (y-1)*data.orig_ancho*data.draw_scale);
-                    var ty = iy + data.iso_basis_y.dot(xyvector);
-                    var tx = ix + data.iso_basis_x.dot(xyvector);
-                    
-                    if (y==1) { 
-                        var draw_z = final_z * data.orig_grosor * data.draw_scale;
-                        for (var z=1; z<draw_z-2; z++) {
-                            this.graphics.lineStyle(1, utils.rnd.pick([0xcec89d, 0xf5dfd1, 0xe5cf81]), 1);
-                            this.graphics.lineBetween(tx, ty+data.dy_B*data.draw_scale+z,tx+data.dx_A*data.draw_scale, ty+data.dy_A*data.draw_scale+z);
-                        }
-                        if (x!=final_x) {
-                            this.graphics.lineStyle(2, 0x553515, 1);
-                            this.graphics.lineBetween(tx+data.dx_A*data.draw_scale, ty+data.dy_A*data.draw_scale,tx+data.dx_A*data.draw_scale, ty+data.dy_A*data.draw_scale+draw_z-2);
-                            //this.graphics.lineBetween(tx+52, ty+24,tx+71, ty+12);
-                        }
-                        
-                    }
-                    
-                    if (x==final_x) { 
-                        this.graphics.fillStyle(0x9a7b5e, 1);
-                        var draw_z = final_z * data.orig_grosor * data.draw_scale;
-                        this.graphics.beginPath();  
-                        this.graphics.moveTo(tx+data.dx_A*data.draw_scale, ty+data.dy_A*data.draw_scale);
-                        this.graphics.lineTo(tx+data.dx_B*data.draw_scale, ty+data.dy_B*data.draw_scale);
-                        this.graphics.lineTo(tx+data.dx_B*data.draw_scale, ty+data.dy_B*data.draw_scale+draw_z-1);
-                        this.graphics.lineTo(tx+data.dx_A*data.draw_scale, ty+data.dy_A*data.draw_scale+draw_z-1);
-
-                        this.graphics.closePath();
-                        this.graphics.fillPath();
-                        if ( y<final_y) {
-                            this.graphics.lineStyle(2, 0x553515, 1);
-                            this.graphics.lineBetween(tx+data.dx_B*data.draw_scale + 1, ty+data.dy_B*data.draw_scale,tx+data.dx_B*data.draw_scale + 1, ty+data.dy_B*data.draw_scale+draw_z-2);
-                            //this.graphics.lineBetween(tx+52, ty+24,tx+71, ty+12);
-                        }
-                    }
-                    
-                    this.gameObject .draw(
-                        assets.billete, tx, ty);
-
-                }
-            } 
-            this.gameObject .draw(this.graphics);
-            this.graphics.clear();
-        };
-        
-        this.gameObject.setData("billetesDims", { x: final_x, y: final_y, z: final_z });
-        
-        return this;
-    },
-    PlotDineros: function (dineros, scene) {
-        var prevMonton = null;
-        this.container = scene.add.container([],0,0);
-        this.montones =  dineros.map(function (d) {
-            var monton = new utils.CuboDeDinero(d.value/100, scene, 0, 0);
-            if (prevMonton != null) {
-                monton.gameObject.x = prevMonton.x + prevMonton.width + 100;
-                monton.gameObject.y = prevMonton.y + prevMonton.height*0.2;
-            }
-            prevMonton = monton;
-            this.container.add(monton);
-            return monton;
-        });
-        
-        return this;
-    }
+    rnd: new Phaser.Math.RandomDataGenerator(0)
 }
+function CuboDeDinero(N, scene, x, y) {
+    
+    var v        =     data.orig_ancho*data.orig_grosor*data.orig_largo, 
+        V        =     N*v, 
+        pre_x    = Math.pow(V, 1/3) / data.orig_largo,
+        final_x  =     Math.max(1, Math.ceil(pre_x)),
+        pre_y    = Math.sqrt( V / (data.orig_largo * final_x) )/data.orig_ancho,
+        final_y  = Math.max(1, Math.ceil(pre_y)),
+        final_z  = Math.ceil(V/(data.orig_grosor * final_x * data.orig_largo * final_y * data.orig_ancho)) ;
+    var ix =0, iy = 0;
+    iy = -data.iso_basis_y.dot(new Phaser.Math.Vector2(0, (final_y-1)*data.orig_ancho*data.draw_scale));
+    var alto  = iy + final_z*data.orig_grosor*data.draw_scale + data.iso_basis_y.dot(new Phaser.Math.Vector2((final_x+1)*data.orig_largo*data.draw_scale, 0)) + 5; 
+    var ancho = data.iso_basis_x.dot(new Phaser.Math.Vector2((final_x)*data.orig_largo*data.draw_scale, (final_y)*data.orig_ancho*data.draw_scale)); 
+    this.gameObject = new Phaser.GameObjects.RenderTexture(scene, x, y, ancho, alto);
+    this.graphics = scene.add.graphics().setVisible(false);
+
+    this.graphics.setTexture("billete_lado");
+    this.graphics.fillStyle(0xdadb5e, 1);
+
+    this.render = function () {
+        for (var x = 1; x <= final_x; x++) {
+            for (var y = 1; y <= final_y; y++) {
+                var xyvector = new Phaser.Math.Vector2((x-1)*data.orig_largo*data.draw_scale,  (y-1)*data.orig_ancho*data.draw_scale);
+                var ty = iy + data.iso_basis_y.dot(xyvector);
+                var tx = ix + data.iso_basis_x.dot(xyvector);
+                
+                if (y==1) { 
+                    var draw_z = final_z * data.orig_grosor * data.draw_scale;
+                    for (var z=1; z<draw_z-2; z++) {
+                        this.graphics.lineStyle(1, utils.rnd.pick([0xcec89d, 0xf5dfd1, 0xe5cf81]), 1);
+                        this.graphics.lineBetween(tx, ty+data.dy_B*data.draw_scale+z,tx+data.dx_A*data.draw_scale, ty+data.dy_A*data.draw_scale+z);
+                    }
+                    if (x!=final_x) {
+                        this.graphics.lineStyle(2, 0x553515, 1);
+                        this.graphics.lineBetween(tx+data.dx_A*data.draw_scale, ty+data.dy_A*data.draw_scale,tx+data.dx_A*data.draw_scale, ty+data.dy_A*data.draw_scale+draw_z-2);
+                        //this.graphics.lineBetween(tx+52, ty+24,tx+71, ty+12);
+                    }
+                    
+                }
+                
+                if (x==final_x) { 
+                    this.graphics.fillStyle(0x9a7b5e, 1);
+                    var draw_z = final_z * data.orig_grosor * data.draw_scale;
+                    this.graphics.beginPath();  
+                    this.graphics.moveTo(tx+data.dx_A*data.draw_scale, ty+data.dy_A*data.draw_scale);
+                    this.graphics.lineTo(tx+data.dx_B*data.draw_scale, ty+data.dy_B*data.draw_scale);
+                    this.graphics.lineTo(tx+data.dx_B*data.draw_scale, ty+data.dy_B*data.draw_scale+draw_z-1);
+                    this.graphics.lineTo(tx+data.dx_A*data.draw_scale, ty+data.dy_A*data.draw_scale+draw_z-1);
+
+                    this.graphics.closePath();
+                    this.graphics.fillPath();
+                    if ( y<final_y) {
+                        this.graphics.lineStyle(2, 0x553515, 1);
+                        this.graphics.lineBetween(tx+data.dx_B*data.draw_scale + 1, ty+data.dy_B*data.draw_scale,tx+data.dx_B*data.draw_scale + 1, ty+data.dy_B*data.draw_scale+draw_z-2);
+                        //this.graphics.lineBetween(tx+52, ty+24,tx+71, ty+12);
+                    }
+                }
+                
+                this.gameObject .draw(
+                    assets.billete, tx, ty);
+
+            }
+        } 
+        this.gameObject .draw(this.graphics);
+        this.graphics.clear();
+    };
+    
+    this.gameObject.setData("billetesDims", { x: final_x, y: final_y, z: final_z });
+    
+    return this;
+}
+function PlotDineros(dineros, scene) {
+      var prevMonton = null;
+      this.container = scene.add.container([],0,0);
+      var that = this;
+      this.montones =  dineros.map(function (d) {
+          var monton = new CuboDeDinero(d.value/100, scene, 0, 0);
+          if (prevMonton != null) {
+              monton.gameObject.x = prevMonton.gameObject.x + prevMonton.gameObject.width*0.75 + 50;
+              monton.gameObject.y = prevMonton.gameObject.y + prevMonton.gameObject.height*1.1 - monton.gameObject.height;
+          }
+          prevMonton = monton;
+          that.container.add(monton.gameObject);
+          monton.render();
+          return monton;
+      });
+      
+      return this;
+  }
 
 var assets = {
   billete: null
@@ -129,6 +132,7 @@ function loader() {
     };
     
     var montonA;
+    var scene;
 
     function preload ()
     {
@@ -144,8 +148,9 @@ function loader() {
 
     function create ()
     {
-				this.cameras.main.setRenderToTexture(this._utils.pipeline);
-				
+        scene = this;
+        this.cameras.main.setRenderToTexture(this._utils.pipeline);
+        
         assets.billete = this.add.image(100, 100, "billete");
         assets.billete.setVisible(false).setScale(data.draw_scale).setOrigin(0,0);
         
@@ -171,7 +176,14 @@ function loader() {
                 padding: {x: 20, y: 10}
             };
           
-        var title07 = this.add.text(0, 850, "2004-2007", titleStyle );
+            
+        var database = Papa.parse("/gte_financiamiento.csv", {
+            download: true,
+            complete: function (results, file) {
+                setDatabase(results.data);
+            }
+        });
+        /*var title07 = this.add.text(0, 850, "2004-2007", titleStyle );
         
         var B = new utils.CuboDeDinero(47e6/100, this, 0,0);
         this.add.existing(B.gameObject);
@@ -198,12 +210,12 @@ function loader() {
         this.add.existing(E.gameObject);
         E.render();
         this.add.text(500,680,"Q 800 000 ", textStyle);
-        
+        */
 				data.scrolling = false;
-				var scene = this;
+				
         scene.input.on('pointerdown', function(pointer){
             data.scrolling = true;
-            data.scrollingOrigin = {x: pointer.x, y: pointer.y, camx: scene.cameras.main.scrollY, camy: scene.cameras.main.scrollY};
+            data.scrollingOrigin = {x: pointer.x, y: pointer.y, camx: scene.cameras.main.scrollX, camy: scene.cameras.main.scrollY};
         });
         scene.input.on('pointerup', function(pointer){
             data.scrolling = false;
@@ -237,28 +249,41 @@ function loader() {
     function setDatabase(database) {
         // Agregar por período
         window.database = database;
-        var aggMain = {};
+        var aggMain = {}, aggArray = [];
         // Columns: ["", "FECHA_APORTE", "AÑO", "MONTO_float", "PARTIDO", "NOMBRE_COMPLETO", "FILIACIÓN"]
         database.forEach(function (row, i) {
-            if (i == 0) return; // skip first row
+            if (i == 0 || row[3] == "NaN") return; // skip first row
             var periodo = Math.floor((row[2]-2004)/4);
             if (aggMain[periodo] === undefined) {
                 aggMain[periodo] = {};
             }
             if (aggMain[periodo][row[4]]  === undefined) {
-                aggMain[periodo][row[4]] = 0;
+                aggMain[periodo][row[4]] =  { value: 0, id: row[4] };
             }
-            aggMain[periodo][row[4]] += parseFloat(row[3]);
+            aggMain[periodo][row[4]].value += parseFloat(row[3]) || 0;
         });
-        console.log(aggMain);
+        
+        // Plot
+        var years =  Object.getOwnPropertyNames(aggMain).map(function (d) {
+            return {
+                "id": d, 
+                "data": Object.getOwnPropertyNames(aggMain[d]).map(function (f) {
+                    return aggMain[d][f];
+                })
+            }
+        });
+        
+        var yearsMontones = years.map(function (d) {
+            if (d.id == 1) {
+                var plot = new PlotDineros(d.data, scene);
+                scene.add.existing(plot.container);
+                return plot;
+            } 
+        });
+        console.log(yearsMontones);
+        
     }
 
-    var database = Papa.parse("/gte_financiamiento.csv", {
-        download: true,
-        complete: function (results, file) {
-            setDatabase(results.data);
-        }
-    });
     var vis = new Phaser.Game(config);
     return vis;
 }
